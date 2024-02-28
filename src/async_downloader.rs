f078dbe472d886b0b7178fa317ca22797d31ae75
warped-brain <110997347+warped-brain@users.noreply.github.com>
2024-02-28 21:41:16 +0530

    add more functionalities
    added maximum connection limit, allows calling function from synchronous
    runtime

--- src/async_downloader.rs
+++ src/async_downloader.rs
@@ -1,187 +1,313 @@
 use bytes::Bytes;
 use reqwest::header::{HeaderValue, CONTENT_LENGTH, RANGE};
-use reqwest::StatusCode;
+use reqwest::{Response, StatusCode};
+use std::borrow::Borrow;
 use std::fs::File;
 use std::io::{self, Read};
+use std::os::unix::fs::FileExt;
 use std::sync::Arc;
 use std::usize;
 use std::{fs::OpenOptions, io::Write};
 use tokio::runtime::Runtime;
 use tokio::sync::mpsc::{self, Receiver, Sender};
-use tokio::sync::Mutex;
-use tokio::task::JoinHandle;
+use tokio::sync::{Mutex, Semaphore};
 #[derive(Debug)]
 pub enum AsyncErrors {
-    INVALID_CHUNK_SIZE,
-    UNREADABLE_BYTES,
-    REQ_FETCH_ERROR,
-    RANGES_UNSUPPORTED,
-    PARSE_INT,
-    UNEXPECTED_SERVER_RESPONSE,
+    InvalidChunkSize,
+    UnreadableBytes,
+    ReqFetchError,
+    RangesUnsupported,
+    ParseInt,
+    UnexpectedServerResponse,
 }
 // Rust Cookbook
 #[derive(Debug)]
 struct PartialRangeIter {
     start: u64,
     end: u64,
-    buffer_size: u32,
+    chunk_size: u32,
 }
 
 impl PartialRangeIter {
-    pub fn new(start: u64, end: u64, buffer_size: u32) -> Result<Self, AsyncErrors> {
-        if buffer_size == 0 {
-            return Err(AsyncErrors::INVALID_CHUNK_SIZE);
+    pub fn new(start: u64, end: u64, chunk_size: u32) -> Result<Self, AsyncErrors> {
+        if chunk_size == 0 {
+            return Err(AsyncErrors::InvalidChunkSize);
         }
         Ok(PartialRangeIter {
             start,
             end,
-            buffer_size,
+            chunk_size,
         })
     }
 }
 
 impl Iterator for PartialRangeIter {
-    type Item = String;
-    fn next(&mut self) -> Option<String> {
+    type Item = (u64, u64);
+    fn next(&mut self) -> Option<(u64, u64)> {
         if self.start > self.end {
             None
         } else {
             let prev_start = self.start;
-            self.start += std::cmp::min(self.buffer_size as u64, self.end - self.start + 1);
-            Some(format!("bytes={}-{}", prev_start, self.start - 1))
+            self.start += std::cmp::min(self.chunk_size as u64, self.end - self.start + 1);
+            Some((prev_start, self.start - 1))
         }
     }
 }
 
-pub struct AsyncDownloader {
-    rt: Runtime,
-    sx: std::sync::mpsc::Sender<(usize, bytes::Bytes)>,
-    failed: Arc<Mutex<Vec<(usize, String)>>>,
-    // rx: tokio::sync::mpsc::Receiver<(usize, bytes::Bytes)>,
+#[derive(Copy, Clone, Debug)]
+pub struct ChunkConfig {
+    pub bytes_range: [u64; 2],
+    pub i: usize,
 }
-impl AsyncDownloader {
+impl ChunkConfig {
+    pub fn chunk_size(&self) -> u64 {
+        self.bytes_range[1] - self.bytes_range[0] + 1
+    }
+    pub fn header_value(&self) -> String {
+        format!("bytes={}-{}", self.bytes_range[0], self.bytes_range[1])
+    }
+}
+
+#[derive(Debug)]
+pub struct DownloadTask {
+    pub chunk_size: usize,
+    pub url: Arc<String>,
+    pub multi_connection: usize,
+    pub chunk_config: Option<ChunkConfig>,
+    pub out_file: File,
+}
+pub struct AsyncDownloader<'a> {
+    rt: &'a Runtime,
+    task_rx: tokio::sync::mpsc::Receiver<DownloadTask>,
+    progress_channel: std::sync::mpsc::Sender<u64>,
+    failed_sender: tokio::sync::mpsc::Sender<DownloadTask>,
+}
+impl<'a> AsyncDownloader<'a> {
     pub fn new(
-        rt: tokio::runtime::Runtime,
-        sx: std::sync::mpsc::Sender<(usize, bytes::Bytes)>,
-        //rx: tokio::sync::mpsc::Receiver<(usize, bytes::Bytes)>,
+        rt: &'a tokio::runtime::Runtime,
+        task_rx: tokio::sync::mpsc::Receiver<DownloadTask>,
+        progress_channel: std::sync::mpsc::Sender<u64>,
+        failed_sender: tokio::sync::mpsc::Sender<DownloadTask>,
     ) -> Self {
         Self {
             rt,
-            sx,
-            failed: Arc::new(Mutex::new(Vec::new())),
+            task_rx,
+            failed_sender,
+            progress_channel,
         }
     }
-    pub async fn async_download_url(
-        self,
-        url: &String,
-        out_file: &String,
+    pub async fn get_tasks(&mut self) {
+        println!("Waitint for tasks");
+        while let Some(task) = self.task_rx.recv().await {
+            let task = Arc::new(task);
+            println!("received task {:?}", *task);
+            self.async_downloadtask_handler(task.clone()).await;
+        }
+    }
+    pub async fn async_downloadtask_handler(
+        &mut self,
+        task: Arc<DownloadTask>,
     ) -> Result<(), AsyncErrors> {
-        match reqwest::blocking::get(url) {
-            Ok(r) => {
-                // println!("{:?}", r.bytes());
-                let _ = std::fs::write(
-                    out_file,
-                    match r.bytes() {
-                        Ok(bytes) => bytes,
-                        Err(_) => return Err(AsyncErrors::UNREADABLE_BYTES),
-                    },
-                );
-                Ok(())
+        let chunk_download_possible = check_partial_downloadable(task.url.clone()).await;
+        match chunk_download_possible {
+            Ok(chunk_download_possible) => {
+                if chunk_download_possible {
+                    self.async_download_url_chunks(task.clone()).await;
+                } else {
+                    println!("Serial Download");
+                    // self.async_serial_download(&task)
+                }
             }
-            Err(e) => Err(AsyncErrors::REQ_FETCH_ERROR),
+            Err(e) => {
+                eprint!("{:?}", e);
+            }
         }
+        Ok(())
     }
-    pub fn async_download_url_chunks(
-        self,
-        url: &String,
-        // out_file: String,
-        chunk_size: u32,
+    pub async fn async_download_url_chunks(
+        &mut self,
+        task: Arc<DownloadTask>,
     ) -> Result<(), AsyncErrors> {
-        // let mut out_file = OpenOptions::new().append(true).open(out_file).unwrap();
-        let client = reqwest::blocking::Client::new();
-        let response = client.head(url).send().expect("Cannot Fetch headers");
+        if (task).chunk_config.is_some() {
+            chunk_downloader(task.clone(), None, self.failed_sender.clone()).await;
+        }
+        // Get content length
+        let client = reqwest::Client::new();
+        let url = (*task.url).clone();
+        let response = client.head(url).send().await.expect("Cannot Fetch headers");
         let length = match response.headers().get(CONTENT_LENGTH) {
             Some(l) => Ok(l),
-            None => Err(AsyncErrors::RANGES_UNSUPPORTED),
+            None => Err(AsyncErrors::RangesUnsupported),
         };
         let length = match length.unwrap().to_str().unwrap().parse::<u64>() {
             Ok(l) => l,
-            Err(_) => return Err(AsyncErrors::PARSE_INT),
+            Err(_) => return Err(AsyncErrors::ParseInt),
         };
+
+        //
+
         let mut handles = Vec::new();
 
-        // let h = std::thread::spawn(|| async_file_aggregator(self.rx, out_file));
-        //println!("starting download...");
-        let mut r_iter = PartialRangeIter::new(0, length - 1, chunk_size).unwrap();
-        for (i, range) in r_iter.enumerate() {
-            let sx = self.sx.clone();
-            let url = url.clone();
-            handles.push(
-                self.rt
-                    .spawn(chunk_downloader(range, i, url, sx, self.failed.clone())),
-            );
-        }
-        while let Some(x) = self.failed.blocking_lock().pop() {
-            let sx = self.sx.clone();
-            let url = url.clone();
+        //Calculate chunk length
+        let connection_limit = Arc::new(Semaphore::new(task.multi_connection));
 
-            handles.push(
-                self.rt
-                    .spawn(chunk_downloader(x.1, x.0, url, sx, self.failed.clone())),
-            );
+        println!("starting download...");
+        let r_iter = PartialRangeIter::new(0, length - 1, task.chunk_size as u32).unwrap();
+        for (i, range) in r_iter.enumerate() {
+            println!("{:?}", connection_limit.available_permits());
+            let task = task.clone();
+            let failed_sender = self.failed_sender.clone();
+            let chunk_config = ChunkConfig {
+                bytes_range: [range.0, range.1],
+                i,
+            };
+            let permit = connection_limit.clone().acquire_owned().await.unwrap();
+            handles.push(self.rt.spawn(async move {
+                chunk_downloader(task.clone(), Some(chunk_config), failed_sender.clone()).await;
+                drop(permit);
+            }));
+            //println!("chunk :{i}");
         }
 
         for handle in handles {
-            self.rt.block_on(handle).unwrap();
+            tokio::join!(handle);
+            println!("joining");
         }
-        // h.join();
-
+        println!("finished downloading");
+        self.task_rx.close();
         Ok(())
+        // h.join();
     }
 }
+
+//Impl end
+//
+//
+
 pub async fn chunk_downloader(
-    range: String,
-    i: usize,
-    url: String,
-    sx: std::sync::mpsc::Sender<(usize, bytes::Bytes)>,
-    failed: Arc<Mutex<Vec<(usize, String)>>>,
+    task: Arc<DownloadTask>,
+    chunk_config: Option<ChunkConfig>,
+    failed_sender: tokio::sync::mpsc::Sender<DownloadTask>,
 ) {
+    if let Some(chunk_config) = chunk_config {
+        // original chunk download
+
+        let f_start: u64 = (chunk_config.i * task.chunk_size) as u64;
+        let client = reqwest::Client::new();
+        let r = client
+            .get((*task.url).clone())
+            .header(RANGE, chunk_config.header_value())
+            .send()
+            .await;
+        match r {
+            Ok(mut response) => {
+                let mut n = 0;
+                while let Some(chunk) = response.chunk().await.unwrap() {
+                    //let chunk = response.bytes().await.unwrap();
+                    task.out_file.write_at(chunk.as_ref(), f_start + n);
+                    n += chunk.len() as u64;
+                    if n < 100000 || n > 900000 {
+                        println!(
+                            "chunk {} n:{n} done {:?}",
+                            &chunk_config.i,
+                            &chunk_config.header_value()
+                        );
+                    }
+                }
+            }
+            Err(_) => {
+                let task: DownloadTask = DownloadTask {
+                    chunk_size: task.chunk_size,
+                    url: task.url.clone(),
+                    multi_connection: task.multi_connection,
+                    chunk_config: Some(chunk_config),
+                    out_file: task.out_file.try_clone().unwrap(),
+                };
+                failed_sender.send(task);
+            }
+        };
+    }
+    // Failed Chunk DownloadTask
+    else {
+        let chunk_config = task.chunk_config.unwrap();
+        let f_start: u64 = (chunk_config.i * task.chunk_size) as u64;
+        let client = reqwest::Client::new();
+        let r = client
+            .get((*task.url).clone())
+            .header(RANGE, chunk_config.header_value())
+            .send()
+            .await;
+        match r {
+            Ok(mut response) => {
+                let n = 0;
+                while let Some(chunk) = response.chunk().await.unwrap() {
+                    task.out_file.write_at(chunk.as_ref(), f_start + n);
+
+                    println!("failed chunk{} n:{n} done", &chunk_config.i);
+                }
+            }
+            Err(a) => {
+                let task: DownloadTask = DownloadTask {
+                    chunk_size: task.chunk_size,
+                    url: task.url.clone(),
+                    multi_connection: task.multi_connection,
+                    chunk_config: Some(chunk_config),
+                    out_file: task.out_file.try_clone().unwrap(),
+                };
+                failed_sender.send(task);
+            }
+        };
+    }
+}
+async fn chunk_response(
+    task: &Arc<DownloadTask>,
+    chunk_config: &ChunkConfig,
+) -> Result<Response, reqwest::Error> {
     let client = reqwest::Client::new();
-    let mut response = match client.get(url).header(RANGE, &range).send().await {
+    client
+        .get((*task.url).clone())
+        .header(RANGE, chunk_config.header_value())
+        .send()
+        .await
+}
+
+async fn check_partial_downloadable(url: Arc<String>) -> Result<bool, AsyncErrors> {
+    let client = reqwest::Client::new();
+    let range = "bytes=0-1";
+    match client.get((*url).clone()).header(RANGE, range).send().await {
         Ok(r) => {
             let status = r.status();
             if !(status == StatusCode::OK || status == StatusCode::PARTIAL_CONTENT) {
-                eprintln!("Failed to fetch {i}th chunk.");
-                failed.lock().await.push((i, range));
-                return;
-            };
-            sx.send((i, r.bytes().await.unwrap()));
-
-            println!("{i} range {:?}", &range);
+                eprintln!("Link is not resumable.");
+                Ok(false)
+            } else {
+                Ok(true)
+            }
         }
         Err(_) => {
-            eprintln!("Failed to fetch {i}th chunk.");
+            eprintln!("Failed to connect.");
+            Err(AsyncErrors::ReqFetchError)
         }
-    };
+    }
 }
 
-pub fn async_file_aggregator(mut rx: std::sync::mpsc::Receiver<(usize, Bytes)>, out_file: String) {
-    println!("Called");
-    let mut rec: Vec<_> = Vec::new();
-    while let Ok(data) = rx.recv() {
-        println!("Received");
-        rec.push(data);
-    }
-    rec.sort_by_key(|item| item.0);
-    println!("Sorted");
-    let mut out_file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .write(true)
-        .open(out_file)
-        .unwrap();
-    for i in rec {
-        let _ = out_file.write(i.1.to_vec().as_slice());
-    }
-    println!("Finished Writing to file");
-}
+// pub fn async_file_aggregator(mut rx: std::sync::mpsc::Receiver<(usize, Bytes)>, out_file: String) {
+//     println!("Called");
+//     let mut rec: Vec<_> = Vec::new();
+//     while let Ok(data) = rx.recv() {
+//         println!("Received");
+//         rec.push(data);
+//     }
+//     rec.sort_by_key(|item| item.0);
+//     println!("Sorted");
+//     let mut out_file = OpenOptions::new()
+//         .create(true)
+//         .append(true)
+//         .write(true)
+//         .open(out_file)
+//         .unwrap();
+//     for i in rec {
+//         let _ = out_file.write(i.1.to_vec().as_slice());
+//     }
+//     println!("Finished Writing to file");
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,50 @@
 use bytes::Bytes;
-use parasync_downloader::async_downloader::{async_file_aggregator, AsyncDownloader};
+use parasync_downloader::{
+    async_downloader::{AsyncDownloader, DownloadTask},
+    sync_downloader::download_url,
+};
 use std::{
+    fs::OpenOptions,
+    sync::Arc,
     thread::{self, JoinHandle, Thread},
     time::Instant,
 };
 use tokio::runtime;
 
 fn main() {
-    let url =
-        "https://static.videezy.com/system/protected/files/000/002/421/heavy_rain_on_tree_branches.mp4".to_string();
+    std::env::set_var("RUST_BACKTRACE", "1");
+    let url = "https://getsamplefiles.com/download/mp4/sample-3.mp4".to_string();
     let out_file = "a.mp4".to_string();
+    let out_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(out_file)
+        .unwrap();
     println!("Async Download Time: ");
     let i = Instant::now();
-    let (mut sx, mut rx) = std::sync::mpsc::channel::<(usize, Bytes)>();
+    let (mut task_sx, mut task_rx) = tokio::sync::mpsc::channel::<DownloadTask>(5);
+    let (mut progress_sx, mut progress_rx) = std::sync::mpsc::channel::<u64>();
+    let downloadtask = DownloadTask {
+        chunk_size: 1_000_000,
+        url: Arc::new(url),
+        multi_connection: 10,
+        chunk_config: None,
+        out_file: out_file,
+    };
+    task_sx.blocking_send(downloadtask);
+
     println!("Started Thread");
-    let h = std::thread::spawn(move || {
-        async_file_aggregator(rx, out_file);
-    });
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(6)
+    let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
-        .unwrap();
-    let ad = AsyncDownloader::new(rt, sx);
-    ad.async_download_url_chunks(&url, 100_000_u32);
+        .expect("Tokio Runtime");
+    let task_sx_1 = task_sx.clone();
 
-    h.join();
+    let mut downloader = AsyncDownloader::new(&rt, task_rx, progress_sx.clone(), task_sx_1);
     println!("Finished");
+    rt.block_on(downloader.get_tasks());
     // download_url_chunks(&url.to_string(), &out_file.to_string(), 100000_u32);
     println!("Time: {:?}", i.elapsed());
-
     // println!("Sync Download Time: ");
     //
     // let i = Instant::now();
@@ -37,4 +52,12 @@ fn main() {
     // println!("Finished");
     // // download_url_chunks(&url.to_string(), &out_file.to_string(), 100000_u32);
     // println!("Time: {:?}", i.elapsed());
+    // tokio::join!();
 }
+
+// pub async fn initialise_downloader(
+//     task_rx: tokio::sync::mpsc::Receiver<DownloadTask>,
+//     progress_sx: std::sync::mpsc::Sender<u64>,
+//     task_sx: tokio::sync::mpsc::Sender<DownloadTask>,
+// ) {
+// }
